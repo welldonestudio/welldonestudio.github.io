f5ebe511d7ab4fdcd86f699d22ff4c4ebd6e6ae7
nahee-park <nahee.developer@gmail.com>
2022-08-25 16:08:28 +0900

    feat: config수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const config = {
   title: 'Welldone Studio Docs by DSRV',
   tagline: 'We build multi-purpose blockchain infrastructure with a user-friendly interface.',
-  url: 'https://master.dihnc19206p60.amplifyapp.com',
+  url: 'https://welldonestudio.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
@@ -17,9 +17,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dsrvlabs', // Usually your GitHub org/user name.
-  projectName: 'welldonestudio', // Usually your repo name.
-
+  organizationName: 'welldonestudio', // Usually your GitHub org/user name.
+  projectName: 'welldonestudio.github.io', // Usually your repo name.
+  deploymentBranch: 'gp-deploy',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
