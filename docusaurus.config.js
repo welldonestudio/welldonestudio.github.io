f5e90a1b0448a83accfd51459ed5d5c2e0ffdf08
nahee-park <nahee.developer@gmail.com>
2022-08-31 16:38:35 +0900

    fix: fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,8 +52,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/welldonestudio/welldonestudio.github.io',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
