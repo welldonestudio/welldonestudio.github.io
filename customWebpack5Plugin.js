30e39cc4a86b76d283af6b69484f67fe09927c69
nahee-park <nahee.developer@gmail.com>
2022-11-04 14:38:18 +0900

    dev: add custom webpack5 plugin

---
+++ customWebpack5Plugin.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line
+module.exports = function () {
+  return {
+    name: 'custom-docusaurus-plugin',
+    // eslint-disable-next-line
+    configureWebpack(config, isServer, utils) {
+      return {
+        resolve: {
+          alias: {
+            path: require.resolve('path-browserify'),
+          },
+          fallback: {
+            crypto: require.resolve('crypto-browserify'),
+            stream: require.resolve('stream-browserify'),
+            buffer: require.resolve('buffer'),
+            path: require.resolve('path-browserify'),
+            fs: false,
+            os: false,
+            net: false,
+            http: require.resolve('http-browserify'),
+            https: require.resolve('https-browserify'),
+            assert: false,
+            url: false,
+            util: require.resolve('util'),
+          },
+        },
+        experiments: {
+          asyncWebAssembly: true,
+        },
+      };
+    },
+  };
+};
