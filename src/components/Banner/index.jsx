2f97d6a1d5df2a7a382e390de0e0d099714bf605
Yoon-Suji <jidl88@ewhain.net>
2023-01-13 17:47:29 +0900

    style: main page banner css

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,22 +5,22 @@ import AddIcon from '@mui/icons-material/Add';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const CONTENTS = {
-  WELLCOME: '🎉  Welldone Wallet is out!',
-  TRY: 'Try a single wallet for all your crypto assets.',
-  NEAR: 'NEAR Migration Helper',
-  NEAR_TRY: 'Try to Move your NEAR asset to a private wallet.',
+    WELLCOME: '🎉  WELLDONE Wallet is out!',
+    TRY: 'Try a single wallet for all your crypto assets.',
+    NEAR: 'NEAR Migration Helper',
+    NEAR_TRY: 'Try to Move your NEAR asset to a private wallet.'
 };
 
 function Banner() {
-  const handleClick = () => {
-    window.open('https://welldonestudio.io/#wallet');
-  };
-  const handleNear = () => {
-    window.open('https://docs.welldonestudio.io/contribution/near-welldone-migration/');
-  }
-  return (
-    <section className={styles.banner_wrapper}>
-      {/* <div onClick={handleClick} className={styles['banner-section']}>
+    const handleClick = () => {
+        window.open('https://welldonestudio.io/#wallet');
+    };
+    const handleNear = () => {
+        window.open('https://docs.welldonestudio.io/contribution/near-welldone-migration/');
+    };
+    return (
+        <section className={styles.banner_wrapper}>
+            {/* <div onClick={handleClick} className={styles['banner-section']}>
         <div className={clsx(styles['banner-contents'], styles['banner-contents-left'])}>
           <span className={styles['contents--wellcome']}>{CONTENTS.WELLCOME}</span><br/>
           <span className={styles['contents--try']}>{CONTENTS.TRY}</span>
@@ -34,15 +34,20 @@ function Banner() {
           <ArrowForwardIcon sx={{fontSize: 24}} className={styles['add--icon']} />
         </div>
       </div> */}
-      <div onClick={handleClick} className={styles['banner-section']}>
-        <div className={clsx(styles['banner-contents'], styles['banner-contents-left'])}>
-          <span className={styles['contents--wellcome']}>{CONTENTS.WELLCOME}</span><br/>
-          <span className={styles['contents--try']}>{CONTENTS.TRY}</span>
-          <AddIcon sx={{fontSize: 24}} className={styles['add--icon']}/>
-        </div>
-      </div>
-    </section>
-  );
+            <div onClick={handleClick} className={styles['banner-section']}>
+                <div className={clsx(styles['banner-contents'], styles['banner-contents-left'])}>
+                    <span className={styles['contents--wellcome']}>
+                        {CONTENTS.WELLCOME}
+                    </span>
+                    <br />
+                    <span className={styles['contents--try']}>
+                        {CONTENTS.TRY}
+                    </span>
+                    <AddIcon sx={{ fontSize: 24 }} className={styles['add--icon']} />
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Banner;
