4be3849d03666248bc4e1ac560315f0677ba0de2
kairoski03 <kairoski03@gmail.com>
2023-02-22 16:32:33 +0900

    WEL-182 Docs - Code Sandbox for Aptos - docusaurus build error related to server-side rendering

--- src/components/CodeSandbox/WalletSelector.tsx
+++ src/components/CodeSandbox/WalletSelector.tsx
@@ -4,8 +4,8 @@ import React from "react";
 import { WelldoneWallet } from "@welldone-studio/aptos-wallet-adapter";
 import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import CodeSandbox from './CodeSandbox';
-
+import loadable from '@loadable/component'
+const CodeSandbox = loadable(() => import('./CodeSandbox'))
 export const WalletSelector = () => {
   const wallets = [new WelldoneWallet(), new PetraWallet()];
   return (
