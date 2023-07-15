8bc94bede6b1235deccce9a77a69efb03cddde35
daoauth <57783762+daoauth@users.noreply.github.com>
2023-07-16 02:02:00 +0900

    update: news

--- src/components/AboutUs/news.tsx
+++ src/components/AboutUs/news.tsx
@@ -1,43 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import Card from '../Card';
+import React from 'react';
+import { Box, Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material';
 import { NewsInfos } from './aboutUs';
 
-function Cards() {
-  const [width, setWidth] = useState<number>(window.innerWidth);
-
-  useEffect(() => {
-    const handleWidth = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleWidth);
-    return () => {
-      window.removeEventListener('resize', handleWidth);
-    };
-  });
-
-  return (
-    <Box
-      style={{ display: 'flex', flexDirection: 'column', position: 'relative' }}
-    >
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '8px' }}>
-        {NewsInfos.map((news, idex) => {
-          return  (<Card
-            key={idex}
-            title={news.title}
-            url={news.url}
-            imageUrl={news.imageUrl}
-            description={news.description}
-            width={width}
-            wide={news.wide}
-          />)        
-        })}
-      </div>
-    </Box>
-  );
-}
-
 export default function News() {
-  return <BrowserOnly>{() => <Cards />}</BrowserOnly>
+  return (
+    <Grid container spacing={1}>
+      {NewsInfos.map((news, idex) => {
+        return  (<Grid
+          key={idex}
+          item
+          sm={news.wide ? 12 : 6}
+          md={news.wide ? 6 : 3}
+        >
+          <Card
+            sx={{
+              background: `url(${news.imageUrl}) no-repeat center`,
+              backgroundSize: 'cover',
+              aspectRatio: `${news.wide ? 2/1 : 1}`
+            }}
+          >
+              <Box
+                sx={{
+                  width: '100%',
+                  height: '100%',
+                  background: 'rgba(0, 0, 0, 0)',
+                  '&:hover': {
+                    background: 'rgba(0, 0, 0, 0.8)',
+                    transition: '0.5s',
+                    '.desc': {
+                      transform: 'translateY(0%)',
+                      transition: '0.5s',
+                    }
+                  },
+                }}
+              >
+                <CardActionArea
+                  sx={{ height: '100%' }}
+                  style={{ textDecoration: 'none' }}
+                  LinkComponent='a'
+                  target='_blank'
+                  href={news.url}
+                  
+                >
+                  <CardContent
+                    className='desc'
+                    sx={{
+                      transform: 'translateY(-100%)',
+                    }}
+                  >
+                    <Typography variant='subtitle1' color="white" >
+                      {news.title}                
+                    </Typography>
+                    <Typography variant='caption' color="white">
+                      {news.description}                
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Box>
+          </Card>
+        </Grid>)        
+      })}
+    </Grid>
+  );
 }
