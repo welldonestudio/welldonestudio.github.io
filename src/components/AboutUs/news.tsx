17637cb7734059a8df533d722fcf551a6c51b1b3
daoauth <57783762+daoauth@users.noreply.github.com>
2024-01-18 14:15:01 +0900

    Update news.tsx

--- src/components/AboutUs/news.tsx
+++ src/components/AboutUs/news.tsx
@@ -1,65 +1,58 @@
 import React from 'react';
-import { Box, Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material';
+import { ImageList, ImageListItem, ImageListItemBar, useMediaQuery, useTheme } from '@mui/material';
 import { NewsInfos } from './aboutUs';
 
 export default function News() {
+  const theme = useTheme()
+  const matchDownMd = useMediaQuery(theme.breakpoints.down('md'))
+
   return (
-    <Grid container spacing={1}>
-      {NewsInfos.map((news, idex) => {
-        return  (<Grid
-          key={idex}
-          item
-          xs={news.wide ? 12 : 6}
-          md={news.wide ? 6 : 3}
+    <ImageList cols={matchDownMd ? 2 : 4} rowHeight={250}>
+      {NewsInfos.map((news, key) => (
+        <ImageListItem
+          key={key}
+          cols={news.wide ? 2 : 1}
+          sx={{
+            background: 'rgba(0, 0, 0, 0)',
+            '&:hover': {
+              border: `solid 3px rgba(0,0,0,0)`,
+              transition: '0.5s',
+              '& .desc': {
+                background: 'rgba(1, 1, 1, 1)',
+                transform: 'translateY(0%)',
+                opacity: 1,
+                transition: '0.5s',
+              }
+            },
+            overflow: 'clip'
+          }}
         >
-          <Card
-            sx={{
-              background: `url(${news.imageUrl}) no-repeat center`,
-              backgroundSize: 'cover',
-              aspectRatio: `${news.wide ? 2/1 : 1}`
-            }}
-          >
-              <Box
-                sx={{
+          <a target='_blank' href={news.url}>
+            <img
+              src={news.imageUrl}
+              alt={news.title}
+              loading="lazy"
+              style={{
+                  position: 'absolute',
+                  top:0,
+                  left: 0,
                   width: '100%',
                   height: '100%',
-                  background: 'rgba(0, 0, 0, 0)',
-                  '&:hover': {
-                    background: 'rgba(0, 0, 0, 0.8)',
-                    transition: '0.5s',
-                    '.desc': {
-                      transform: 'translateY(0%)',
-                      transition: '0.5s',
-                    }
-                  },
-                }}
-              >
-                <CardActionArea
-                  sx={{ height: '100%' }}
-                  style={{ textDecoration: 'none' }}
-                  LinkComponent='a'
-                  target='_blank'
-                  href={news.url}
-                  
-                >
-                  <CardContent
-                    className='desc'
-                    sx={{
-                      transform: 'translateY(-100%)',
-                    }}
-                  >
-                    <Typography variant='subtitle1' color="white" >
-                      {news.title}                
-                    </Typography>
-                    <Typography variant='caption' color="white">
-                      {news.description}                
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Box>
-          </Card>
-        </Grid>)        
-      })}
-    </Grid>
+                  objectFit: 'cover'
+              }}
+            />
+            <ImageListItemBar
+              className='desc'
+              title={news.title}
+              subtitle={news.description}
+              sx={{
+                transform: 'translateY(100%)',
+                opacity: 0,
+              }}
+            />
+          </a>
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 }
