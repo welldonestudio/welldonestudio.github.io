b0d81b264036e9fb1961c3d4447d840709d19fb2
daoauth <57783762+daoauth@users.noreply.github.com>
2024-01-23 00:36:44 +0900

    Update news.tsx

--- src/components/AboutUs/news.tsx
+++ src/components/AboutUs/news.tsx
@@ -1,63 +1,68 @@
 import React from 'react';
 import { Button, Divider, ImageList, ImageListItem, ImageListItemBar, useMediaQuery, useTheme } from '@mui/material';
-import { NewsInfos } from './aboutUs';
+import { News, NewsInfos } from './aboutUs';
 
 export default function News() {
   const theme = useTheme()
   const matchDownMd = useMediaQuery(theme.breakpoints.down('md'))
   const [isOpen, setIsopen] = React.useState<boolean>(false);
 
+  const DrawImage = ({item}: {item: News}) => {
+    return (
+      <ImageListItem
+        cols={item.cols || 1}
+        rows={item.rows || 1}
+        sx={{
+          background: 'rgba(0, 0, 0, 0)',
+          '&:hover': {
+            '& .image': {
+              transform: 'scale(1.2)',
+              transition: '1s',
+            },
+            '& .desc': {
+              background: 'rgba(1, 1, 1, 1)',
+              transform: 'translateY(0%)',
+              opacity: 1,
+              transition: '0.5s',
+            }
+          },
+          overflow: 'clip'
+        }}
+      >
+        <a target='_blank' href={item.url}>
+          <img
+            className='image'
+            src={item.imageUrl}
+            alt={item.title}
+            loading="lazy"
+            style={{
+                position: 'absolute',
+                top:0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover'
+            }}
+          />
+          <ImageListItemBar
+            className='desc'
+            title={item.title}
+            subtitle={item.description}
+            sx={{
+              transform: 'translateY(100%)',
+              opacity: 0,
+            }}
+          />
+        </a>
+      </ImageListItem>
+    );
+  }
+
   return (
     <>
       <ImageList cols={matchDownMd ? 2 : 4} rowHeight={250} variant="quilted">
         {NewsInfos.pined.map((item, key) => (
-          <ImageListItem
-            key={key}
-            cols={item.cols || 1}
-            rows={item.rows || 1}
-            sx={{
-              background: 'rgba(0, 0, 0, 0)',
-              '&:hover': {
-                '& .image': {
-                  transform: 'scale(1.2)',
-                  transition: '1s',
-                },
-                '& .desc': {
-                  background: 'rgba(1, 1, 1, 1)',
-                  transform: 'translateY(0%)',
-                  opacity: 1,
-                  transition: '0.5s',
-                }
-              },
-              overflow: 'clip'
-            }}
-          >
-            <a target='_blank' href={item.url}>
-              <img
-                className='image'
-                src={item.imageUrl}
-                alt={item.title}
-                loading="lazy"
-                style={{
-                    position: 'absolute',
-                    top:0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover'
-                }}
-              />
-              <ImageListItemBar
-                className='desc'
-                title={item.title}
-                subtitle={item.description}
-                sx={{
-                  transform: 'translateY(100%)',
-                  opacity: 0,
-                }}
-              />
-            </a>
-          </ImageListItem>
+          <DrawImage key={key} item={item} />
         ))}
       </ImageList>
       <div
@@ -90,53 +95,7 @@ export default function News() {
           style={{ marginTop: 12 }}
         >
           {NewsInfos.more.map((item, key) => (
-            <ImageListItem
-              key={key}
-              cols={1}
-              rows={1}
-              sx={{
-                background: 'rgba(0, 0, 0, 0)',
-                '&:hover': {
-                  '& .image': {
-                    transform: 'scale(1.2)',
-                    transition: '1s',
-                  },
-                  '& .desc': {
-                    background: 'rgba(1, 1, 1, 1)',
-                    transform: 'translateY(0%)',
-                    opacity: 1,
-                    transition: '0.5s',
-                  }
-                },
-                overflow: 'clip'
-              }}
-            >
-              <a target='_blank' href={item.url}>
-                <img
-                  className='image'
-                  src={item.imageUrl}
-                  alt={item.title}
-                  loading="lazy"
-                  style={{
-                      position: 'absolute',
-                      top:0,
-                      left: 0,
-                      width: '100%',
-                      height: '100%',
-                      objectFit: 'cover'
-                  }}
-                />
-                <ImageListItemBar
-                  className='desc'
-                  title={item.title}
-                  subtitle={item.description}
-                  sx={{
-                    transform: 'translateY(100%)',
-                    opacity: 0,
-                  }}
-                />
-              </a>
-            </ImageListItem>
+            <DrawImage key={key} item={item} />
           ))}
         </ImageList>
       </div>
