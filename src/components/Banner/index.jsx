d14827e17fad43a921b0ee938a4c55c9863fe839
daoauth <57783762+daoauth@users.noreply.github.com>
2024-02-05 19:01:41 +0900

    Update index.jsx

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -16,7 +16,7 @@ const CONTENTS = {
 
 function Banner() {
     const handleClick = () => {
-        window.open('http://localhost:3000/tutorials/sui-zklogin/');
+        window.open('http://docs.welldonestudio.io/tutorials/sui-zklogin/');
     };
     const handleNear = () => {
         window.open('https://docs.welldonestudio.io/contribution/near-welldone-migration/');
@@ -51,11 +51,30 @@ function Banner() {
             </div>
         </div>
       */}
-            <div className={styles['banner-section']} style={{ paddingLeft: 0, marginTop: '32px' }} onClick={handleClick}>
-                <Chip label="New" color="primary" size='small' style={{ position: 'absolute', top: '-12px', left: '40px' }}/>
+            <Box
+                style={{
+                    maxWidth: '1108px',
+                    minWidth: '100px',
+                    border: '1px solid #566077',
+                    cursor: 'pointer',
+                    borderRadius: '8px',
+                }}
+                sx={{
+                    width: {
+                        xs: '100%',
+                        sm: 'calc(100% - 10px * 2)'
+                    },
+                    marginTop: {
+                        xs: '16px',
+                        sm: '32px'
+                    },
+                }}
+                onClick={handleClick}
+            >
+                <Chip label="New" color="primary" size='small' sx={{ position: 'absolute', top: {xs: '45px', sm: '60px'}, left: { xs: '20px', sm: '40px'} }}/>
                 <span className={styles['contents--wellcome']}>
-                    <Box style={{ padding: '40px', display: 'flex', justifyContent: 'space-between' }}>
-                        <Box width="194px">
+                    <Box style={{ display: 'flex', justifyContent: 'space-between' }} sx={{ padding: { xs: '20px', md: '40px' } }}>
+                        <Box sx={{ width: { xs: "129px", sm: '194px' }}}>
                             <img src={'/img/zkwallet.svg'}/>
                         </Box>
                         <Box style={{ display: 'flex',  alignItems: 'center' }}>
@@ -63,19 +82,19 @@ function Banner() {
                                 style={{
                                     background: 'linear-gradient(270deg, #FF912C 24%, #2D6CFF 78%, #6AB8FF 100%)',
                                     backgroundClip: 'text',
-                                    WebkitTextFillColor: 'transparent'
+                                    WebkitTextFillColor: 'transparent',
+                                    lineHeight: '120%'
                                 }}
-                                sx={{ display: { xs: 'none', sm: 'none', md: 'block' }}}
+                                sx={{ fontSize: { xs: '14px', sm: '24px', textAlign: 'end' }}}
                             >
                                 {CONTENTS.ZKWALLET}
                             </Box>
                             <ArrowForwardIcon sx={{ fontSize: 24, display: { xs: 'none', sm: 'block' }, marginLeft: '16px' }} />   
                         </Box>
                     </Box>
-                    
                 </span>
                 <Player video={require('@site/static/video/zkwallet.mp4').default} />
-            </div>
+            </Box>
         </section>
     );
 }
